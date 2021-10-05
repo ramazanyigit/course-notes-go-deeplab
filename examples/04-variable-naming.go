c2846cffc1d8132e96ad5210d153c84039dee26f
ramazanyigit <54190298+ramazanyigit@users.noreply.github.com>
2021-10-05 20:52:57 +0300

    Add variable accessor example source file.

---
+++ examples/04-variable-naming.go
@@ -0,0 +1,13 @@
+package main
+
+// Private variable.
+// If a variable starts with a non-capital letter, it will have private accessor.
+var name string = "golang"
+
+// Public variable.
+// If a variable starts with a capital letter, it will have public accessor.
+var Version string = "1.2.3"
+
+func main() {
+
+}
